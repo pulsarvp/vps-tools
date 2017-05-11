7b857a287e6c5377968efdca57d57ed650d105ce
witzawitz <witzawitz@gmail.com>
2017-05-11 18:02:40 +0300

    Method for recreating views.

--- src/controllers/DbController.php
+++ src/controllers/DbController.php
@@ -1,6 +1,7 @@
 <?php
 	namespace vps\tools\controllers;
 
+	use vps\tools\helpers\Console;
 	use Yii;
 
 	/**
@@ -9,6 +10,31 @@
 	class DbController extends \yii\console\Controller
 	{
 		public $defaultAction = 'tables';
+
+		/**
+		 * List all views. This method is needed in order to match the database collection if it was changed.
+		 */
+		public function actionRecreateViews ()
+		{
+			$db = Yii::$app->db;
+			$views = $this->getViews();
+
+			foreach ($views as $view)
+			{
+				$info = $db->createCommand("SHOW CREATE VIEW `$view`")->queryAll();
+				$sql = $info[ 0 ][ 'Create View' ];
+				preg_match("/.*VIEW `$view` as (.*)/i", $sql, $match);
+				if (isset($match[ 1 ]))
+				{
+					$db->createCommand("CREATE OR REPLACE VIEW `$view` AS " . $match[ 1 ])->execute();
+					Console::printColor("View `$view` was recreated.", "green");
+				}
+				else
+				{
+					Console::printColor("Cannot find create string for view `$view`.", "red");
+				}
+			}
+		}
 
 		/**
 		 * List all tables in database.
