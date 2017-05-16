0c3d4ed1c1622c191abf93e45bf75ff559a60257
Kuteiko <kuteiko@mail.ru>
2017-05-16 17:11:02 +0300

    Remove default charset table

--- src/modules/rbac/migrations/m010203_400005_init_rbac.php
+++ src/modules/rbac/migrations/m010203_400005_init_rbac.php
@@ -1,5 +1,5 @@
 <?php
-	use vps\tools\db\Migration;
+	use yii\db\Migration;
 
 	/**
 	 * Class m010101_100001_init_rbac
@@ -9,12 +9,18 @@
 		/** @inheritdoc */
 		public function up ()
 		{
+			$tableOptions = null;
+			if ($this->db->driverName === 'mysql')
+			{
+				$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+			}
+
 			$this->createTable('auth_rule', [
 				'name'       => $this->string(64)->notNull(),
 				'data'       => $this->text()->null(),
 				'created_at' => $this->integer()->null(),
 				'updated_at' => $this->integer()->null()
-			]);
+			], $tableOptions);
 			$this->addPrimaryKey('name', 'auth_rule', 'name');
 
 			$this->createTable('auth_item', [
@@ -25,7 +31,7 @@
 				'data'        => $this->text()->null(),
 				'created_at'  => $this->integer()->null(),
 				'updated_at'  => $this->integer()->null()
-			]);
+			], $tableOptions);
 			$this->addPrimaryKey('name', 'auth_item', 'name');
 
 			$this->createIndex('type', 'auth_item', 'type');
@@ -36,7 +42,7 @@
 			$this->createTable('auth_item_child', [
 				'parent' => $this->string(64)->notNull(),
 				'child'  => $this->string(64)->notNull()
-			]);
+			], $tableOptions);
 			$this->addPrimaryKey('parent', 'auth_item_child', [ 'parent', 'child' ]);
 
 			$this->createIndex('child', 'auth_item_child', 'child');
@@ -48,7 +54,7 @@
 				'item_name'  => $this->string(64)->notNull(),
 				'user_id'    => $this->integer()->notNull(),
 				'created_at' => $this->integer()->null()
-			]);
+			], $tableOptions);
 			$this->addPrimaryKey('item_name', 'auth_assignment', [ 'item_name', 'user_id' ]);
 
 			$this->createIndex('user_id', 'auth_assignment', 'user_id');
@@ -75,7 +81,7 @@
 			$auth->add($unverified);
 
 			$this->renameColumn("user", 'isApproved', 'active');
-			
+
 			$userClass = Yii::$app->getModule('rbac')->modelUser;
 			$users = $userClass::find()->all();
 			foreach ($users as $user)
