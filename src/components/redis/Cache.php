70e153fcf307a5acdbbab2912be385a707c080bb
Kuteiko <kuteiko@mail.ru>
2019-11-15 18:32:48 +0300

    Настройка forceClusterMode для кеша Redis

--- src/components/redis/Cache.php
+++ src/components/redis/Cache.php
@@ -6,6 +6,23 @@
 
 	class Cache extends \yii\redis\Cache
 	{
+
+		/**
+		 * Class Cache
+		 *
+		 * @package       vps\tools\components\redis
+		 *
+		 *
+		 * ```php
+		 *
+		 * 'cache'     => [
+		 *    'class'            => 'vps\tools\components\redis\Cache',
+		 *    'keyPrefixDb'      => 'cache_redis_key_prefix',
+		 *    'forceClusterModeDb'       => 'cache_redis_force_cluster',
+		 *    ],
+		 * ```
+		 */
+		
 		/**
 		 * Set keyPrefix from DB settings.
 		 * @param string $name
@@ -13,5 +30,18 @@
 		public function setKeyPrefixDb ($name)
 		{
 			$this->keyPrefix = Yii::$app->settings->get($name);
+		}
+
+		/**
+		 * Set forceClusterMode from DB settings.
+		 * @param string $name
+		 */
+		public function setForceClusterModeDb ($name)
+		{
+			$item = Yii::$app->settings->get($name);
+			if ($item === 1 or $item === true)
+				$this->forceClusterMode = true;
+			else
+				$this->forceClusterMode = false;
 		}
 	}
